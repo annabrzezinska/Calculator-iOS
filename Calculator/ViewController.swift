08921d8a08fd92c18e030957737cbef3136e3843
Anna Brzezińska <anna.dorota.brzezinska@gmail.com>
2023-09-02 17:47:59 +0200

    Initial project setup

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,19 +1,3 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Anna Brzezińska on 02/09/2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+class ViewController: UIViewController {}
