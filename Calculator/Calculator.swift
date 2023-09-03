13f3ffb9fcc811d2c2286f59d2a97fe4681a3496
Anna Brzezińska <anna.dorota.brzezinska@gmail.com>
2023-09-03 14:12:39 +0200

    Refactor for readability and remove nested if statement

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -6,37 +6,36 @@ func calculate(input: [String]) -> String {
     }
     
     let operators: [String] = ["+", "-", "*", "/"]
-    let result: [String]
-    let hasMoreThanOneOperator = input.filter({element in operators.contains(element)}).count > 1
-    let lastOperator = input.last(where: { element in operators.contains(element) })
+    let hasMoreThanOneOperator = input.filter({ element in operators.contains(element) }).count > 1
     let firstOperator = input.first(where: { element in operators.contains(element) })
+    let lastOperator = input.last(where: { element in operators.contains(element) })
+    let isLastElementOperator = operators.contains(lastElement)
+    let result: [String]
     
-    if operators.contains(lastElement) {
-        if let `operator` = firstOperator, hasMoreThanOneOperator {
-            let inputWithoutLastOperator = input.dropLast()
-            let expression = inputWithoutLastOperator.split(separator: `operator`)
-            let leftNumberString = expression[0].joined()
-            let rightNumberString = expression[1].joined()
-            
-            let leftNumber = try! Decimal.init(leftNumberString, format: .number)
-            let rightNumber = try! Decimal.init(rightNumberString, format: .number)
-            let mathResult: Decimal
-            
-            if `operator` == "+" {
-                mathResult = leftNumber + rightNumber
-            } else if `operator` == "-" {
-                mathResult = leftNumber - rightNumber
-            } else if `operator` == "*"{
-                mathResult = leftNumber * rightNumber
-            } else {
-                mathResult = leftNumber / rightNumber
-            }
-            result = ["\(mathResult)"]
+    if let `operator` = firstOperator, hasMoreThanOneOperator, isLastElementOperator {
+        let inputWithoutLastOperator = input.dropLast()
+        let expression = inputWithoutLastOperator.split(separator: `operator`)
+        let leftNumberString = expression[0].joined()
+        let rightNumberString = expression[1].joined()
+        
+        let leftNumber = try! Decimal.init(leftNumberString, format: .number)
+        let rightNumber = try! Decimal.init(rightNumberString, format: .number)
+        let mathResult: Decimal
+        
+        if `operator` == "+" {
+            mathResult = leftNumber + rightNumber
+        } else if `operator` == "-" {
+            mathResult = leftNumber - rightNumber
+        } else if `operator` == "*"{
+            mathResult = leftNumber * rightNumber
         } else {
-            result = input.dropLast()
+            mathResult = leftNumber / rightNumber
         }
+        
+        result = ["\(mathResult)"]
     } else if let `operator` = lastOperator {
-        let splitResult = input.split(separator: `operator`, maxSplits: 1)
+        let splitResult = input.split(separator: `operator`)
+        
         result = Array(splitResult.last!)
     } else {
         result = input
